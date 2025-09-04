9fe510d198894b5ba99c2e803f62e6a841b5761d
Couch187 <eleanor.hinton@ucas-edu.net>
2025-09-04 13:43:51 -0600

    Started Expressions Notes

---
+++ notes/expressions.py
@@ -0,0 +1,47 @@
+# EH 6th Expressions Notes!
+
+# What is an algorithm 
+    # A set of instructions to accomplish a specific task. 
+name = input("What is your name? ")
+print("Hello", name)
+
+#algorithm for area
+length = 5 
+width = 20 
+area = length * width 
+print(length, "x", width, "=", area)
+
+#1
+age_one = 13
+age_two = 14
+age_three = 15
+age_four = 14
+#2
+total = age_one + age_two + age_three + age_four 
+average = total/4
+#3
+print("The average age between:", age_one, ",", age_two, ",", age_three, ", and", age_four, "=", average)
+
+#Math equations! 
+num_one = 12
+num_two = 4 
+
+print("addition:", num_one + num_two)
+print("subtraction:", num_one - num_two)
+print("multlipication:", num_one * num_two)
+print("division:", num_one / num_two)
+print("exponents:", num_one ** num_two)
+print("integer division:", num_one // num_two)
+print("modulo (remainder):", num_one % num_two)
+
+# List steps in an algorithm
+    # To create an algorithm, 
+
+
+# List ALL of the different assignment operators (Give me the symbols and what it does)
+    #
+
+
+# Why are expressions so important in programming?
+    #
+
